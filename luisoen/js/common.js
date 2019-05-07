f7cdc917679eea0cb3b0fba4b3129b82e23be7c8
etozhunya <49534738+etozhunya@users.noreply.github.com>
2019-05-07 21:24:12 +0300

    ready

--- luisoen/js/common.js
+++ luisoen/js/common.js
@@ -5,4 +5,26 @@ $(document).ready(function(){
 	dots: false,
 	fade: true
   });
+
+  $('a[href^="#"]').click(function(e) {
+  	e.preventDefault();
+  	var id = $(this).attr('href');
+  	$('body,html').animate(function() {
+  		scrollTop: $(id).offset().top
+  	}, 600)
+
+  	if ($(this).hasClass('mob-link')) {
+  		$('.header_mob-nav').removeClass('active');
+  	}
+  })
+
+  $('.hamburger').click(function () {
+  	if ($('.header_mob-nav').hasClass('active')) {
+  		$('.header_mob-nav, .hamburger').removeClass('active');
+  	} else {
+  		$('.header_mob-nav, .hamburger').addClass('active');
+  	}
+  	
+  })
 });
+ 
