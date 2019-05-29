287151ff5dcff27b5429954e6267657a10f0c1ce
etozhunya <49534738+etozhunya@users.noreply.github.com>
2019-05-29 20:35:57 +0300

    ready??

--- Aavio/js/common.js
+++ Aavio/js/common.js
@@ -1,12 +1,40 @@
-var hamburger = document.getElementById('hamburger'),
+$(document).ready(function(){
+
+  if ($(window).width() < 768) {
+
+  	$('.advisors-team').slick({
+  	  	// infinite: true,
+ 		centerMode: true,
+    	slidesToShow: 1,
+    	centerPadding: "70px"
+  	});
+
+  	  	$('.team').slick({
+  	  	// infinite: true,
+ 		centerMode: true,
+    	slidesToShow: 1,
+    	centerPadding: "80px"
+  	});
+
+  	  	$('.ul-legacy').slick({
+  	  	// infinite: true,
+ 		centerMode: true,
+    	slidesToShow: 1,
+    	centerPadding: "0px"
+  	});
+  }
+
+
+  var hamburger = document.getElementById('hamburger'),
 	menu = document.querySelector('.top-header_nav');
 
-// hamburger.onclick = function(){
-// 	hamburger.classList.toggle('visible')
-// }
+	// hamburger.onclick = function(){
+	// 	hamburger.classList.toggle('visible')
+	// }
+	
+	hamburger.addEventListener('click', function() {
+		hamburger.classList.toggle('active');
+		menu.classList.toggle('visible');
+	})
 
-hamburger.addEventListener('click', function() {
-	hamburger.classList.toggle('active');
-	menu.classList.toggle('visible');
-})
-
+});
