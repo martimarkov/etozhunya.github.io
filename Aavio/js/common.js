d0120ce40ac80cc19c76ac8bd0a5b789794e3a71
etozhunya <49534738+etozhunya@users.noreply.github.com>
2019-06-26 16:50:10 +0300

    last

--- Aavio/js/common.js
+++ Aavio/js/common.js
@@ -60,10 +60,6 @@ $(document).ready(function(){
 
   var hamburger = document.getElementById('hamburger'),
 	menu = document.querySelector('.top-header_nav');
-
-	// hamburger.onclick = function(){
-	// 	hamburger.classList.toggle('visible')
-	// }
 	
 	hamburger.addEventListener('click', function() {
 		hamburger.classList.toggle('active');
