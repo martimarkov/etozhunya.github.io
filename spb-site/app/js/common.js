8f7ddc4bdf13b7fa41c1a8ddd775741c5f53cef2
etozhunya <49534738+etozhunya@users.noreply.github.com>
2019-06-28 00:21:17 +0300

    desktop 80%

--- spb-site/app/js/common.js
+++ spb-site/app/js/common.js
@@ -1,1 +1,20 @@
-
+$(document).ready(function () {
+  var $select = $('#select2-id'), $message = $('#message');
+  $select.select2();
+  
+  $select.on('change', function () {
+    $message.html('I heard a global change event');
+  });
+  
+  $('#change-trigger').on('click', function () {
+    // Reset Message
+    $message.html('&nbsp;');
+    $select.val('two').trigger('change');
+  });
+  
+  $('#change-select2-trigger').on('click', function () {
+    // Reset Message
+    $message.html('&nbsp;');
+    $select.val('three').trigger('change.select2');
+  });
+});
