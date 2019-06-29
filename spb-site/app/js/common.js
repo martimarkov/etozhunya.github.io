0f446b037d559214e1d9c929caadd93ed7fee7fc
etozhunya <49534738+etozhunya@users.noreply.github.com>
2019-06-30 01:35:44 +0300

    top100

--- spb-site/app/js/common.js
+++ spb-site/app/js/common.js
@@ -17,4 +17,13 @@ $(document).ready(function () {
     $message.html('&nbsp;');
     $select.val('three').trigger('change.select2');
   });
+
+  $('#top-people').click(function() {
+    $('.top100').addClass('in-view');
+    $('.top100').removeClass('hide');
+  });
+  $('#popular').click(function() {
+    $('.top100').removeClass('in-view');
+    $('.top100').addClass('hide');
+  });
 });
