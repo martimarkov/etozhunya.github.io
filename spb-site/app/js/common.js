563a02fb0dd902d352a2de44dfcad85f026033dd
etozhunya <49534738+etozhunya@users.noreply.github.com>
2019-07-01 00:18:37 +0300

    ul people JS+

--- spb-site/app/js/common.js
+++ spb-site/app/js/common.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
+
+  $('.top100').addClass('in-view');
+  $('.popular').addClass('hide');
+  $('.recent').addClass('hide');
+  $('#top-people').css({
+    "font-family": "Mweather-b"
+  })
   var $select = $('#select2-id'), $message = $('#message');
   $select.select2();
   
@@ -18,12 +25,74 @@ $(document).ready(function () {
     $select.val('three').trigger('change.select2');
   });
 
+
   $('#top-people').click(function() {
+    $('#recent').css({
+      "font-family": "Mweather"
+    })
+    $('#top-people').css({
+      "font-family": "Mweather-b"
+    })
+    $('#popular').css({
+      "font-family": "Mweather"
+    })
+
     $('.top100').addClass('in-view');
     $('.top100').removeClass('hide');
+    $('.popular').removeClass('in-view');
+    $('.popular').addClass('hide');
+    $('.recent').removeClass('in-view');
+    $('.recent').addClass('hide');
   });
+
+
   $('#popular').click(function() {
+    $('#top-people').css({
+      "font-family": "Mweather"
+    })
+    $('#popular').css({
+      "font-family": "Mweather-b"
+    })
+    $('#recent').css({
+      "font-family": "Mweather"
+    })
+    $('.top100').removeClass('in-view');
+    $('.popular').removeClass('hide');
+    $('.top100').addClass('hide');
+    $('.popular').addClass('in-view');
+    $('.recent').removeClass('in-view');
+    $('.recent').addClass('hide');
+  });
+
+
+  $('#recent').click(function() {
+    $('#popular').css({
+      "font-family": "Mweather"
+    })
+    $('#top-people').css({
+      "font-family": "Mweather"
+    })
+    $('#recent').css({
+      "font-family": "Mweather-b"
+    })
+
     $('.top100').removeClass('in-view');
     $('.top100').addClass('hide');
+    $('.popular').removeClass('in-view');
+    $('.popular').addClass('hide');
+    $('.recent').removeClass('hide');
+    $('.recent').addClass('in-view');
   });
+
+  $('.general-nav li').click(function() {
+    var index = $(this).attr('data-index');
+
+    $('.general-nav .rectangle_gradient').css({
+      "transform": "translate3d(" + index + "00%, 0, 0)"
+    })
+    $('.general-nav .rectangle_gradient').css({
+      "transition": ".5s"
+    })
+  })
+
 });
