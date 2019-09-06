b6114266223d25e545d8aa394ca9078ae4a91587
etozhunya <49534738+etozhunya@users.noreply.github.com>
2019-09-06 22:07:26 +0300

    done

--- skoro_sdam/app/js/common.js
+++ skoro_sdam/app/js/common.js
@@ -1,1 +1,8 @@
-
+$(window).scroll(function() {
+	if ($(window).scrollTop() > 860) {
+		$('.header-fixed').addClass('active')
+	}
+	if ($(window).scrollTop() < 860) {
+		$('.header-fixed').removeClass('active')
+	}
+})
