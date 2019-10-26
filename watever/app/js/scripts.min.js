33fff3f6bd83ded3db930692dd1102455fedcd8f
etozhunya <49534738+etozhunya@users.noreply.github.com>
2019-10-26 18:07:16 +0300

    ис

--- watever/app/js/scripts.min.js
+++ watever/app/js/scripts.min.js
@@ -1,1 +1,10 @@
+var hamburger = document.getElementById('hamburger'),
+menu = document.querySelector('.menu');
 
+hamburger.addEventListener('click', function() {
+	hamburger.classList.toggle('active');
+	menu.classList.toggle('hidden');
+})
+if ($(window).width() < 768) {	
+	$('.menu').addClass('hidden');	
+}
