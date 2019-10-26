94e786d961429681d4f07c73acebfa13cf80003e
etozhunya <49534738+etozhunya@users.noreply.github.com>
2019-10-27 02:00:27 +0300

    check

--- watever/app/js/common.js
+++ watever/app/js/common.js
@@ -8,3 +8,11 @@ hamburger.addEventListener('click', function() {
 if ($(window).width() < 768) {	
 	$('.menu').addClass('hidden');	
 }
+$(function() {
+
+  $('.what').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 800, 'linear');
+
+	});
+});
