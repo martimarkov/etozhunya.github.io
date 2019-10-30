33950f5b08842b95c075224cdf3ea1e6919648f0
etozhunya <49534738+etozhunya@users.noreply.github.com>
2019-10-30 03:40:27 +0200

    dasda
    ss

--- watever/app/js/common.js
+++ watever/app/js/common.js
@@ -1,21 +1,50 @@
-var hamburger = document.getElementById('hamburger'),
-menu = document.querySelector('.menu');
-
-hamburger.addEventListener('click', function() {
-	hamburger.classList.toggle('active');
-	menu.classList.toggle('hidden');
-})
-if ($(window).width() < 768) {	
-	$('.menu').addClass('hidden');	
-}
 $(function() {
+	var hamburger = document.getElementById('hamburger'),
+	menu = document.querySelector('.menu');
+
+	hamburger.addEventListener('click', function() {
+		hamburger.classList.toggle('active');
+		menu.classList.toggle('hidden');
+	})
+	if ($(window).width() < 768) {	
+		$('.menu').addClass('hidden');
+		$('.links').on('click', function() {
+			$('.menu').addClass('hidden');
+		})
+	}
+
 
   $('.what').on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 800, 'linear');
 
 	});
+  $('.up').on('click', function() {
+  	$('html, body').animate({ scrollTop:0}, 400, 'linear');
+  })
+  $('.down').on('click', function() {
+  	$('html, body').animate({ scrollTop:$(document).height() - $(window).height()}, 400, 'linear');
+  })
+
+	$(window).scroll(function(){
+		if ($(window).scrollTop() == 0) {
+			$('.up').addClass('hide')
+		}
+		if ($(window).width() > 768) {
+			if ($(window).scrollTop() == 0) {
+				$('.menu').addClass('toTop');
+			}	
+		}
+		if ($(window).scrollTop() > 5) {
+			$('.up').removeClass('hide')
+			$('.menu').removeClass('toTop')
+		}
+		if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+			$('.down').addClass('hide');
+		} else {
+			$('.down').removeClass('hide');
+		}
+		console.log($(document).height() - $(window).height())
+		console.log($(window).scrollTop())
+	});
 });
-// $('.links').onClick() {
-// 	$('.menu').addClass('hidden');
-// }
