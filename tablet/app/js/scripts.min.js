67c9fa43d84661290f09b8528b7084689d85273f
etozhunya <49534738+etozhunya@users.noreply.github.com>
2019-11-18 03:03:29 +0200

    asdasda

--- tablet/app/js/scripts.min.js
+++ tablet/app/js/scripts.min.js
@@ -8,119 +8,123 @@ function isMobileDevice() {
 
  
 var li = new Array();
-function crear_select(){
-var div_cont_select = document.querySelectorAll("[data-mate-select='active']");
-var select_ = '';
-for (var e = 0; e < div_cont_select.length; e++) {
-div_cont_select[e].setAttribute('data-indx-select',e);
-div_cont_select[e].setAttribute('data-selec-open','false');
-var ul_cont = document.querySelectorAll("[data-indx-select='"+e+"'] > .cont_list_select_mate > ul");
- select_ = document.querySelectorAll("[data-indx-select='"+e+"'] >select")[0];
- if (isMobileDevice()) { 
-select_.addEventListener('change', function () {
- _select_option(select_.selectedIndex,e);
-});
- }
-var select_optiones = select_.options;
-document.querySelectorAll("[data-indx-select='"+e+"']  > .selecionado_opcion ")[0].setAttribute('data-n-select',e);
-document.querySelectorAll("[data-indx-select='"+e+"']  > .icon_select_mate ")[0].setAttribute('data-n-select',e);
-for (var i = 0; i < select_optiones.length; i++) {
-li[i] = document.createElement('li');
-if (select_optiones[i].selected == true || select_.value == select_optiones[i].innerHTML ) {
-li[i].className = 'active';
-document.querySelector("[data-indx-select='"+e+"']  > .selecionado_opcion ").innerHTML = select_optiones[i].innerHTML;
-};
-li[i].setAttribute('data-index',i);
-li[i].setAttribute('data-selec-index',e);
-// funcion click al selecionar 
-li[i].addEventListener( 'click', function(){  _select_option(this.getAttribute('data-index'),this.getAttribute('data-selec-index')); });
 
-li[i].innerHTML = select_optiones[i].innerHTML;
-ul_cont[0].appendChild(li[i]);
+function crear_select() {
+    var div_cont_select = document.querySelectorAll("[data-mate-select='active']");
+    var select_ = '';
+    for (var e = 0; e < div_cont_select.length; e++) {
+        div_cont_select[e].setAttribute('data-indx-select', e);
+        div_cont_select[e].setAttribute('data-selec-open', 'false');
+        var ul_cont = document.querySelectorAll("[data-indx-select='" + e + "'] > .cont_list_select_mate > ul");
+        select_ = document.querySelectorAll("[data-indx-select='" + e + "'] >select")[0];
+        if (isMobileDevice()) {
+            // select_.addEventListener('change', function() {
+                // _select_option(select_.selectedIndex, e);
+            // });
+        }
+        var select_optiones = select_.options;
+        document.querySelectorAll("[data-indx-select='" + e + "']  > .selecionado_opcion ")[0].setAttribute('data-n-select', e);
+        document.querySelectorAll("[data-indx-select='" + e + "']  > .icon_select_mate ")[0].setAttribute('data-n-select', e);
+        for (var i = 0; i < select_optiones.length; i++) {
+            li[i] = document.createElement('li');
+            if (select_optiones[i].selected == true || select_.value == select_optiones[i].innerHTML) {
+                li[i].className = 'active';
+                document.querySelector("[data-indx-select='" + e + "']  > .selecionado_opcion ").innerHTML = select_optiones[i].innerHTML;
+            };
+            li[i].setAttribute('data-index', i);
+            li[i].setAttribute('data-selec-index', e);
+            // funcion click al selecionar 
+            li[i].addEventListener('click', function() {
+                _select_option(this.getAttribute('data-index'), this.getAttribute('data-selec-index'));
+            });
 
-    }; // Fin For select_optiones
-  }; // fin for divs_cont_select
+            li[i].innerHTML = select_optiones[i].innerHTML;
+            ul_cont[0].appendChild(li[i]);
+
+        }; // Fin For select_optiones
+    }; // fin for divs_cont_select
 } // Fin Function 
 
 
 
 var cont_slc = 0;
-function open_select(idx){
-var idx1 =  idx.getAttribute('data-n-select');
-  var ul_cont_li = document.querySelectorAll("[data-indx-select='"+idx1+"'] .cont_select_int > li");
-var hg = 0;
-var slect_open = document.querySelectorAll("[data-indx-select='"+idx1+"']")[0].getAttribute('data-selec-open');
-var slect_element_open = document.querySelectorAll("[data-indx-select='"+idx1+"'] select")[0];
- if (isMobileDevice()) { 
-  if (window.document.createEvent) { // All
-  var evt = window.document.createEvent("MouseEvents");
-  evt.initMouseEvent("mousedown", false, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-  slect_element_open.dispatchEvent(evt);
-} else if (slect_element_open.fireEvent) { // IE
-  slect_element_open.fireEvent("onmousedown");
-}else {
-  slect_element_open.click();
-}
-}else {
 
-  
-  for (var i = 0; i < ul_cont_li.length; i++) {
-hg += ul_cont_li[i].offsetHeight;
-}; 
- if (slect_open == 'false') {  
- document.querySelectorAll("[data-indx-select='"+idx1+"']")[0].setAttribute('data-selec-open','true');
- document.querySelectorAll("[data-indx-select='"+idx1+"'] > .cont_list_select_mate > ul")[0].style.height = hg+"px";
- document.querySelectorAll("[data-indx-select='"+idx1+"'] > .icon_select_mate")[0].style.transform = 'rotate(180deg)';
-}else{
- document.querySelectorAll("[data-indx-select='"+idx1+"']")[0].setAttribute('data-selec-open','false');
- document.querySelectorAll("[data-indx-select='"+idx1+"'] > .icon_select_mate")[0].style.transform = 'rotate(0deg)';
- document.querySelectorAll("[data-indx-select='"+idx1+"'] > .cont_list_select_mate > ul")[0].style.height = "0px";
- }
-}
+function open_select(idx) {
+    var idx1 = idx.getAttribute('data-n-select');
+    var ul_cont_li = document.querySelectorAll("[data-indx-select='" + idx1 + "'] .cont_select_int > li");
+    var hg = 0;
+    var slect_open = document.querySelectorAll("[data-indx-select='" + idx1 + "']")[0].getAttribute('data-selec-open');
+    var slect_element_open = document.querySelectorAll("[data-indx-select='" + idx1 + "'] select")[0];
+    // if (isMobileDevice()) {
+    //     if (window.document.createEvent) { // All
+    //         var evt = window.document.createEvent("MouseEvents");
+    //         evt.initMouseEvent("mousedown", false, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    //         slect_element_open.dispatchEvent(evt);
+    //     } else if (slect_element_open.fireEvent) { // IE
+    //         slect_element_open.fireEvent("onmousedown");
+    //     } else {
+    //         slect_element_open.click();
+    //     }
+    // } else {
+
+
+        for (var i = 0; i < ul_cont_li.length; i++) {
+            hg += ul_cont_li[i].offsetHeight;
+        };
+        if (slect_open == 'false') {
+            document.querySelectorAll("[data-indx-select='" + idx1 + "']")[0].setAttribute('data-selec-open', 'true');
+            document.querySelectorAll("[data-indx-select='" + idx1 + "'] > .cont_list_select_mate > ul")[0].style.height = hg + "px";
+            document.querySelectorAll("[data-indx-select='" + idx1 + "'] > .icon_select_mate")[0].style.transform = 'rotate(180deg)';
+        } else {
+            document.querySelectorAll("[data-indx-select='" + idx1 + "']")[0].setAttribute('data-selec-open', 'false');
+            document.querySelectorAll("[data-indx-select='" + idx1 + "'] > .icon_select_mate")[0].style.transform = 'rotate(0deg)';
+            document.querySelectorAll("[data-indx-select='" + idx1 + "'] > .cont_list_select_mate > ul")[0].style.height = "0px";
+        }
+    // }
 
 } // fin function open_select
 
-function salir_select(indx){
-var select_ = document.querySelectorAll("[data-indx-select='"+indx+"'] > select")[0];
- document.querySelectorAll("[data-indx-select='"+indx+"'] > .cont_list_select_mate > ul")[0].style.height = "0px";
-document.querySelector("[data-indx-select='"+indx+"'] > .icon_select_mate").style.transform = 'rotate(0deg)';
- document.querySelectorAll("[data-indx-select='"+indx+"']")[0].setAttribute('data-selec-open','false');
+function salir_select(indx) {
+    var select_ = document.querySelectorAll("[data-indx-select='" + indx + "'] > select")[0];
+    document.querySelectorAll("[data-indx-select='" + indx + "'] > .cont_list_select_mate > ul")[0].style.height = "0px";
+    document.querySelector("[data-indx-select='" + indx + "'] > .icon_select_mate").style.transform = 'rotate(0deg)';
+    document.querySelectorAll("[data-indx-select='" + indx + "']")[0].setAttribute('data-selec-open', 'false');
 }
 
 
-function _select_option(indx,selc){
- if (isMobileDevice()) { 
-selc = selc -1;
-}
-    var select_ = document.querySelectorAll("[data-indx-select='"+selc+"'] > select")[0];
+function _select_option(indx, selc) {
+    if (isMobileDevice()) {
+        // selc = selc - 1;
+    }
+    var select_ = document.querySelectorAll("[data-indx-select='" + selc + "'] > select")[0];
 
-  var li_s = document.querySelectorAll("[data-indx-select='"+selc+"'] .cont_select_int > li");
-  var p_act = document.querySelectorAll("[data-indx-select='"+selc+"'] > .selecionado_opcion")[0].innerHTML = li_s[indx].innerHTML;
-var select_optiones = document.querySelectorAll("[data-indx-select='"+selc+"'] > select > option");
-for (var i = 0; i < li_s.length; i++) {
-if (li_s[i].className == 'active') {
-li_s[i].className = '';
-};
-li_s[indx].className = 'active';
+    var li_s = document.querySelectorAll("[data-indx-select='" + selc + "'] .cont_select_int > li");
+    var p_act = document.querySelectorAll("[data-indx-select='" + selc + "'] > .selecionado_opcion")[0].innerHTML = li_s[indx].innerHTML;
+    var select_optiones = document.querySelectorAll("[data-indx-select='" + selc + "'] > select > option");
+    for (var i = 0; i < li_s.length; i++) {
+        if (li_s[i].className == 'active') {
+            li_s[i].className = '';
+        };
+        li_s[indx].className = 'active';
 
-};
-select_optiones[indx].selected = true;
-  select_.selectedIndex = indx;
-  select_.onchange();
-  salir_select(selc); 
+    };
+    select_optiones[indx].selected = true;
+    select_.selectedIndex = indx;
+    select_.onchange();
+    salir_select(selc);
 }
 $('#form').submit(function() {
     formSubmit($(this));
     $('form').addClass('active');
     $('.labels label .input').val('')
-   });
+});
 $('.google_form').submit(function(e) {
     e.preventDefault();
     formSubmit($(this));
     $('.submit_bottom').addClass('active');
     $(this).children('input').val('')
     alert("Thank you for joining Tablet's mailing list! We will be in touch.");
-   });
+});
 $(function() {
 	var hamburger = document.getElementById('hamburger'),
 	menu = document.querySelector('.menu');
@@ -176,15 +180,15 @@ $(function() {
 });
 $('#postal-code, #tel-code').on('keypress change blur', function () {
     $(this).val(function (index, value) {
-        return value.replace(/[^a-z0-9]+/gi, '').replace(/(.{3})/g, '$1 ');
+        return value.replace(/[^a-z0-9]+/gi, '').replace(/(.{3})/g, '$1 ').trim();
     });
 });
 
-$('#postal-code').on('copy cut paste', function () {
-    setTimeout(function () {
-        $('#postal-code').trigger("change");
-    });
-});
+// $('#postal-code').on('copy cut paste', function () {
+//     setTimeout(function () {
+//         $('#postal-code').trigger("change");
+//     });
+// });
 $('.google_form').submit(function(e) {
     e.preventDefault();
     formSubmit($(this));
@@ -235,14 +239,38 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById('tel-code').oninput = function(e) {
             e.target.setCustomValidity("");
         };
+
+        var letters;
+
         document.getElementById('postal-code').oninvalid = function(e) {
             e.target.setCustomValidity("");
+            if(letters == 'M6A' || letters == 'M6B' || letters == 'M6E') {
+              e.target.setCustomValidity("We currently do not deliver in that area. For a list of serviceable areas, please see our Help Center");
+            } else {
+              e.target.setCustomValidity("");
+            }
             if (!e.target.validity.valid) {
-                e.target.setCustomValidity(" Please enter a valid postal code");
+
+              letters = $(this).val().slice(0,3);
+               if(!(letters == 'M6A' || letters == 'M6B' || letters == 'M6E')) {
+                e.target.setCustomValidity("Please enter a valid postal code");
+              }
+            } else {
+              e.target.setCustomValidity("");
             }
         };
         document.getElementById('postal-code').oninput = function(e) {
             e.target.setCustomValidity("");
+
+            letters = $(this).val().slice(0,3);
+            if(letters == 'M6A' || letters == 'M6B' || letters == 'M6E') {
+              e.target.setCustomValidity("We currently do not deliver in that area. For a list of serviceable areas, please see our Help Center");
+            } else if (!e.target.validity.valid) {
+              e.target.setCustomValidity("Please enter a valid postal code");
+            } else {
+              e.target.setCustomValidity("");
+            }
+
         };
 })
 function readURL(input) {
