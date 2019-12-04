4c8d021afee813723aad58f98aef6c34a251d1a8
etozhunya <49534738+etozhunya@users.noreply.github.com>
2019-12-04 07:35:17 +0200

    dasdada

--- blue/app/js/scripts.min.js
+++ blue/app/js/scripts.min.js
@@ -1,19 +1,38 @@
+$(function() {
+	var hamburger = document.getElementById('hamburger'),
+	menu = document.querySelector('.menu');
 
-setInterval(function() {
+	hamburger.addEventListener('click', function() {
+		hamburger.classList.toggle('active');
+		menu.classList.toggle('hidden');
+	})
+	if ($(window).width() < 768) {	
+		$('.menu').addClass('hidden');
+		$('.links').on('click', function() {
+			$('.menu').addClass('hidden');
+		})
+	}  
+});
+ 	$('.visible-phone').on('click', function(e) {
+	    e.preventDefault();
+	    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top},800, 'linear');
+	});
 
-	for ( i = 0; i < $('.wrapper li').length; i++) {	
-		if ($($('.wrapper li')[i]).hasClass('white')) {
-			$($('.wrapper li')[i]).removeClass('white')
-		} else {
-			$($('.wrapper li')[i]).addClass('white');
-		}
-		if (i = $('.wrapper li').length; i++) {
-			setTimeout(function() {
-				i = 0
-			}, 2000)
-		}
-	}
-},1500)
+// setInterval(function() {
+
+// 	for ( i = 0; i < $('.wrapper li').length; i++) {	
+// 		if ($($('.wrapper li')[i]).hasClass('white')) {
+// 			$($('.wrapper li')[i]).removeClass('white')
+// 		} else {
+// 			$($('.wrapper li')[i]).addClass('white');
+// 		}
+// 		if (i = $('.wrapper li').length; i++) {
+// 			setTimeout(function() {
+// 				i = 0
+// 			}, 2000)
+// 		}
+// 	}
+// },1500)
 
 // В нем 
 // for(i=0; i<$(ul).length; i++) {
