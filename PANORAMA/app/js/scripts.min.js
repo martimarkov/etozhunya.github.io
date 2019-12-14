ce3e7a9d76611b5dd9b4fc4b87be88f8baa13d9c
etozhunya <49534738+etozhunya@users.noreply.github.com>
2019-12-15 00:10:23 +0200

    dasd

--- PANORAMA/app/js/scripts.min.js
+++ PANORAMA/app/js/scripts.min.js
@@ -1,11 +1,16 @@
+<script>
 $(document).ready(function() {
     $("#collection-category-474").click(function() {
-        window.open("https://academy.panoramaed.com/collection/474-student-success");
+    	window.location = "https://academy.panoramaed.com/collection/474-student-success";
     });
     $("#collection-category-464").click(function() {
-        window.open("https://academy.panoramaed.com/collection/464-social-emotional-learning-feedback-surveys");
+	   	window.location = "https://academy.panoramaed.com/collection/464-social-emotional-learning-feedback-surveys";
     });
     $("#collection-category-483").click(function() {
-        window.open("https://academy.panoramaed.com/article/553-learning-paths");
+    	window.location = "https://academy.panoramaed.com/article/553-learning-paths";
+    });
+    $("#collection-474 a, #collection-464 a, #collection-483 a").click(function(event) {
+        event.preventDefault();
     });
 });
+</script>
