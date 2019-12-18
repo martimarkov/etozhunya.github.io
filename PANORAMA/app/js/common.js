486ccef7f07402d51fd99d1cf97a8b186013e15b
etozhunya <49534738+etozhunya@users.noreply.github.com>
2019-12-18 17:46:11 +0200

    dasd

--- PANORAMA/app/js/common.js
+++ PANORAMA/app/js/common.js
@@ -1,4 +1,4 @@
-<script>
+// <script>
 $(document).ready(function() {
     $("#collection-category-474").click(function() {
     	window.location = "https://academy.panoramaed.com/collection/474-student-success";
@@ -9,8 +9,23 @@ $(document).ready(function() {
     $("#collection-category-483").click(function() {
     	window.location = "https://academy.panoramaed.com/article/553-learning-paths";
     });
-    $("#collection-474 a, #collection-464 a, #collection-483 a").click(function(event) {
+    $("#collection-category-526").click(function() {
+        window.location = "https://academy.panoramaed.com/collection/526-browse-our-faqs";
+    });
+    $("#collection-category-564").click(function() {
+        window.location = "https://academy.panoramaed.com/article/573-join-a-community-workshop";
+    });
+    $("#collection-category-560").click(function() {
+        window.location = "https://academy.panoramaed.com/article/563-submit-a-success-story";
+    });
+    $("#collection-category-567").click(function() {
+        window.location = "https://www.panoramaed.com/blog/tag/research";
+    });
+    $("#collection-category-570").click(function() {
+        window.location = "https://www.panoramaed.com/resources/success-stories";
+    });
+    $("#collection-category-567 a, #collection-category-570 a, #collection-category-560 a, #collection-474 a, #collection-category-564 a, #collection-category-526 a, #collection-464 a, #collection-483 a").click(function(event) {
         event.preventDefault();
     });
 });
-</script>
+// </script>
