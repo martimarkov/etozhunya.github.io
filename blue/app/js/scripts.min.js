ac7470ecbfd5d5037da9003df31c26d1674712f2
etozhunya <49534738+etozhunya@users.noreply.github.com>
2020-01-26 00:04:34 +0200

    das

--- blue/app/js/scripts.min.js
+++ blue/app/js/scripts.min.js
@@ -12,35 +12,41 @@ $(function() {
 			$('.menu').addClass('hidden');
 		})
 	}  
+	 	$('.visible-phone').on('click', function(e) {
+		    e.preventDefault();
+		    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top},800, 'linear');
+		});
+		if ($(window).width() < 768) {  
+		  $('.href').on('click', function() {
+		    $('.menu').addClass('hidden');
+		    $('#hamburger').removeClass('active');
+		  })
+		}
+	var i = 0;
+	setInterval(function() {
+		if ($($('.wrapper li')[i]).hasClass('white')) {
+			$($('.wrapper li')[i]).removeClass('white')
+		} else {
+			$($('.wrapper li')[i]).addClass('white');
+		}
+
+		if (i == 2) {
+			// setTimeout(function(){
+				i = 0;
+
+			// },1000)
+			
+		} else {
+			i++;
+		}
+	},2000)
+
+	$('.cut').shave(180);
+	if ($(window).width() < 768) {	
+		$('.cut').shave(190);
+	}
 });
- 	$('.visible-phone').on('click', function(e) {
-	    e.preventDefault();
-	    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top},800, 'linear');
-	});
-	if ($(window).width() < 768) {  
-	  $('.href').on('click', function() {
-	    $('.menu').addClass('hidden');
-	    $('#hamburger').removeClass('active');
-	  })
-	}
-var i = 0;
-setInterval(function() {
-	if ($($('.wrapper li')[i]).hasClass('white')) {
-		$($('.wrapper li')[i]).removeClass('white')
-	} else {
-		$($('.wrapper li')[i]).addClass('white');
-	}
 
-	if (i == 2) {
-		// setTimeout(function(){
-			i = 0;
-
-		// },1000)
-		
-	} else {
-		i++;
-	}
-},2000)
 
 
 
