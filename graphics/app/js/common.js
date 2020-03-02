fe9b7a38ddb9213227d41bc861f4dd51f5d2d856
etozhunya <49534738+etozhunya@users.noreply.github.com>
2020-03-02 16:12:45 +0200

    Graphics update

---
+++ graphics/app/js/common.js
@@ -0,0 +1,50 @@
+$(function() {
+	var hamburger = document.getElementById('menu_click'),
+	menu = document.querySelector('.menu');
+
+	hamburger.addEventListener('click', function() {
+		hamburger.classList.toggle('active');
+		menu.classList.toggle('hidden');
+	})
+	if ($(window).width() < 768) {	
+		$('.menu').addClass('hidden');
+		$('.links').on('click', function() {
+			$('.menu').addClass('hidden');
+		})
+	}  
+	 	$('.visible-phone').on('click', function(e) {
+		    e.preventDefault();
+		    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top},800, 'linear');
+		});
+		if ($(window).width() < 768) {  
+		  $('.href').on('click', function() {
+		    $('.menu').addClass('hidden');
+		    $('#menu_click').removeClass('active');
+		  })
+		}
+	var i = 0;
+	setInterval(function() {
+		if ($($('.wrapper li')[i]).hasClass('white')) {
+			$($('.wrapper li')[i]).removeClass('white')
+		} else {
+			$($('.wrapper li')[i]).addClass('white');
+		}
+
+		if (i == 2) {
+			// setTimeout(function(){
+				i = 0;
+
+			// },1000)
+			
+		} else {
+			i++;
+		}
+	},2000)
+
+	$(window).trigger('resize');
+});
+
+
+
+
+
